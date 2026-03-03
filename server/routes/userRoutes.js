debe41f97617dc7311d597b15a806602730f0496
MohammedAqeel18 <aqeelworkmail@gmail.com>
2026-03-03 18:15:49 +0530

    Feat: implemented  authMiddleware for protected routes

--- server/routes/userRoutes.js
+++ server/routes/userRoutes.js
@@ -1,9 +1,13 @@
 import express from "express";
 import {registerUser,loginUser} from "../controller/userController.js";
-
+import { protect} from "../middleware/authMiddleware.js";
 const router = express.Router();
 
 router.post("/register", registerUser)
 router.post("/login", loginUser);
+
+router.get("/profile", protect, async(req,res)=>{
+    res.json(req.user);
+})
 
 export default router;
