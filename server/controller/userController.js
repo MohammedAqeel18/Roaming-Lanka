b983f914fc0a603c0ddb9da52ac82359ed63beaa
MohammedAqeel18 <aqeelworkmail@gmail.com>
2026-03-03 16:58:46 +0530

    Feat: Intergrate JWT authentication for for secure login

--- server/controller/userController.js
+++ server/controller/userController.js
@@ -1,4 +1,5 @@
 import User from "../models/userModel.js";
+import generateToken from "../utils/generateToken.js";
 
 
 export const registerUser = async (req,res)=>{
@@ -29,5 +30,23 @@ export const registerUser = async (req,res)=>{
 
     }else{
         res.status(400).json({message:"Invalid user data"})
+    }
+}
+
+export const loginUser = async (req,res)=>{
+    const {email,password} = req.body;
+
+    const user = await User.findOne({email});
+
+    if(user && (await user.matchPassword(password))){
+        res.json({
+            _id:user._id,
+            name:user.name,
+            email:user.email,
+            isAdmin:user.isAdmin,
+            token:generateToken(user._id),
+        })
+    }else{
+        res.status(401).json({message:"Invalid email or password"})
     }
 }
