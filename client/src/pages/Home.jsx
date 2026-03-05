096a573866d0af3f7ca92fe066563ee5483adda5
MohammedAqeel18 <aqeelworkmail@gmail.com>
2026-03-05 13:51:58 +0530

    Feat: Connected frontend - backend using axios and fetched districts data from database

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,8 +1,36 @@
+import { useEffect,useState } from "react";
+import API from "../services/api";
+
 function Home(){
+
+    const [districts,setDistricts] = useState([])
+
+    useEffect(()=>{
+        const fetchDistricts = async()=>{
+            try{
+                const {data} = await API.get("/districts")
+
+                setDistricts(data.districts)
+            }catch(error){
+                console.error(error)
+            }
+        };
+
+        fetchDistricts();
+    },[])
     return (
         <div>
             <h2> Welcome to Roaming Lanka</h2>
             <p> Discover the beauty of Sri Lanka</p>
+
+            {districts.map((district)=>(
+                <div key={district._id}>
+                 <h2>{district.name} </h2>
+                 <h3> {district.province}</h3>
+                 <h4> {district.description}</h4>   
+                 <h4> {district.rating}</h4>
+                </div>
+            ))}
         </div>
     )
 }
