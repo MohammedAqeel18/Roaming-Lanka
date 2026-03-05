902a47d4d58a6bcdd02c96a847d181fe22396d68
MohammedAqeel18 <aqeelworkmail@gmail.com>
2026-03-05 10:53:36 +0530

    Feat: Initialize frontend with vite and project structure

---
+++ client/src/pages/Home.jsx
@@ -0,0 +1,10 @@
+function Home(){
+    return (
+        <div>
+            <h2> Welcome to Roaming Lanka</h2>
+            <p> Discover the beauty of Sri Lanka</p>
+        </div>
+    )
+}
+
+export default Home;
