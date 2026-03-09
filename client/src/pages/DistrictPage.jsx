0459fee556ede41ad509e04b3d1b5c0a99292047
MohammedAqeel18 <aqeelworkmail@gmail.com>
2026-03-09 19:48:38 +0530

    Feat:Implemented Review System Frontend for reponsive user reviews

--- client/src/pages/DistrictPage.jsx
+++ client/src/pages/DistrictPage.jsx
@@ -40,10 +40,57 @@ function DistrictPage(){
         {district.description}    
         </p>
 
+          <div className="mt-10">
+          <h2 className="text-2xl font-bold mb-4"> Reviews </h2>
+
+          {district.reviews.length === 0 && (
+            <p> No reviews yet</p>
+          )}  
+
+          {district.reviews.map((review, index)=>(
+            <div key={index} className="border p-4 rounded-lg mb-3">
+                <h4 className="font-semibold"> {review.name}</h4>
+
+                <p className="text-gray-700"> {review.rating}</p>
+            
+            <p className="text-gray-700"> {review.comment}</p>
+            </div>
+          ))}
+          </div>
         <p className="mt-4 text-yellow-500 font-bold">
        ⭐  {district.rating}   
         </p>
+
+        <div className="mt-10">
+          <h2 className="text-2xl font-bold mb-4"> Write a review</h2>
+        
+        <form className="space-y-4">
+          <div>
+            <label className="block mb-1"> Rating </label>
+
+            <select className="border p-2 rounded w-full">
+
+               <option value=""> Select Rating</option>
+               <option value="1"> 1- Poor</option>
+               <option value="2"> 2 - Fair</option>
+               <option value="3"> 3 - Good</option>
+               <option value="4">4 - Very Good</option>
+               <option value="5"> 5 - Excellent</option> 
+            </select>
+          </div>
+
+          <div>
+            <label className="block mb-1"> Comment</label>
+          
+          <textarea className="border p-2 rounded w-full " rows="4"/> 
+          </div>
+
+          <button type="submit" className="bg-blue-600 text-white px-4 py-2 rounded"> Submit Review </button>
+        </form>
+
         </div>
+        </div>
+        
     )
 }
 
