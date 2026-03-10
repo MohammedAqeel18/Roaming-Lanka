d2d827aa1164e3824b726e84a03e5a71566c5457
MohammedAqeel18 <aqeelworkmail@gmail.com>
2026-03-10 14:05:04 +0530

    Feat: Created & Implemented Login UI for User Authentication

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,13 +1,14 @@
 import { Routes,Route } from "react-router-dom";
 import Home from "./pages/Home";
 import DistrictPage from "./pages/DistrictPage";
+import Login from "./pages/LoginPage";
 function App(){
   return (
     <Routes>
       
       <Route path="/" element={<Home/>}/>
       <Route path="/district/:id" element={<DistrictPage/>}/>
-            
+      <Route path="login" element={<Login/>}/>
 
     </Routes>
 
---
+++ client/src/pages/LoginPage.jsx
@@ -0,0 +1,54 @@
+import { useState } from "react";
+import API from "../services/api";
+
+function Login(){
+    const [email, setEmail]  = useState("");
+    const [password,setPassword] = useState("");
+
+    const submitHandler = async (e)=>{
+        e.preventDefault();
+
+        try{
+
+            const {data} = await API.get("userInfo", JSON.stringify(data));
+
+            alert("Login Successful");
+
+        } catch(error){
+            console.error(error)
+            alert("Invalid email or password")
+        }
+    };
+
+    return(
+        <div className=" max-w-md mx-auto mt-10">
+            <h1 className="text-3xl font-bold mb-6 text-center">  Login</h1>
+
+            <form className="space-y-4" onSubmit={submitHandler}>
+                <div>
+                 <label> Email</label>
+
+                <input
+                type="Email"
+                className="border w-full p-2 rounded"
+                value={email}
+                placeholder="enter your email"
+                onChange={(e)=>setEmail(e.target.value)}
+                />
+            
+                <input
+                type="password"
+                placeholder="enter your password"
+                value={password}
+                onChange={(e)=>setPassword(e.target.value)}
+                className="border w-full p-2 rounded"
+                />
+                <button type="submit" className="bg-blue-600 text-white w-full py-2 rounded"> Login</button>
+                </div>
+            </form>
+        </div>
+    )
+
+}
+
+export default Login;
