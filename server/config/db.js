944e8171b70f0efc3142a8fc7ab153e50468cabe
MohammedAqeel18 <aqeelworkmail@gmail.com>
2026-06-15 21:44:17 +0530

    feat:fix config file bugs

--- server/config/db.js
+++ server/config/db.js
@@ -5,7 +5,8 @@ const connectDB = async ()=>{
         const  conn =  await mongoose.connect(process.env.MONGODB_URI);
         console.log(`MongoDB connected: ${conn.connection.host}`);
     }catch(error){
-        console.error("Database connection failed", error.message)
+        console.error("Database connection failed");
+        console.error(error);
         process.exit(1);
     }
 }
